04570f49359333c201858f478d04e3b9c2c5e700
pklouros <teste>
2017-10-07 12:05:13 +0000

    Added task qxc

---
+++ taskqcxTransactions.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+// 
+$paths = array("/model");
+$isDevMode = true;
+//
+//require_once __DIR__.'./src/Object.php';
+require_once __DIR__ . '/src/OrderRepository.php';
+require_once __DIR__ . '/src/TopTen.php';
+require_once __DIR__ . '/src/OrderItem.php';
+require_once __DIR__ . '/src/Order.php';
+require_once __DIR__ . '/src/Currency.php';
+require_once __DIR__ . '/src/CustomerOrders.php';
+require_once __DIR__ . '/src/MyETHPrices.php';
+require_once __DIR__ . '/src/mybtcprices.php';
+require_once __DIR__ . '/src/openqcxorders.php';
+require_once __DIR__ . '/src/qcxTransactions.php';
+
+
+//// the connection configuration
+require 'config.inc';
+// 
+//
+//
+//start the connection with db.
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/src"), $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+//----
+
+
+
+
+$nonce     = time(); // Unix timestamp
+$key       = 'hmNgRNnDNC'; // My API Key
+$client    = 47301; // My Client ID
+$book		= 'btc_cad'; //specify the currency
+$secret    = '99c933d1cedd7799b88215e9f201b3ad'; // My secret
+$signature = hash_hmac('sha256', $nonce . $client . $key, $secret); // Hashing it
+$data = array(
+    'key'       => $key,
+    'nonce'     => $nonce,
+    'signature' => $signature
+);
+$data_string = json_encode($data);
+$ch = curl_init('https://api.quadrigacx.com/v2/balance');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json; charset=utf-8',
+    'Content-Length: ' . strlen($data_string))
+);
+$result = curl_exec($ch);
+ $result = json_decode($result, true);
+ var_dump($result);
+ $data = $result[0];
+var_dump($data);
+
+
+$list = $entityManager->getRepository('\src\qcxTransactions')->
+$btc =  new src\qcxTransactions($data); 
+
+    $entityManager->persist($btc);
+    
+    $entityManager->flush();   
+           
+    curl_close($ch);
