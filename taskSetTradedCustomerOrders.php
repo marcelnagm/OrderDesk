acfaa2996400156fa662432191466e1dffd9a5ad
Marcel Nagm <marcel.nagm@gmail.com>
2017-10-13 11:21:26 -0400

    task 7 item 4

---
+++ taskSetTradedCustomerOrders.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+// 
+$paths = array("/model");
+$isDevMode = true;
+//
+//require_once __DIR__.'./src/Object.php';
+require_once __DIR__ . '/src/OrderRepository.php';
+require_once __DIR__ . '/src/TopTen.php';
+require_once __DIR__ . '/src/Shipping.php';
+require_once __DIR__ . '/src/Shipment.php';
+require_once __DIR__ . '/src/OrderItem.php';
+require_once __DIR__ . '/src/Order.php';
+require_once __DIR__ . '/src/Currency.php';
+require_once __DIR__ . '/src/CustomerOrders.php';
+require_once __DIR__ . '/src/MyETHPrices.php';
+require_once __DIR__ . '/src/mybtcprices.php';
+
+
+//// the connection configuration
+require './config.inc';
+// 
+//
+//
+//start the connection with db.
+$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/src"), $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
+
+$entityManager->getConnection()->exec('UPDATE customerorders set Traded = 1 WHERE ExternalOrderID in (select order_id from  qcxtransactionhistory) and ExternalOrderID != ""');
